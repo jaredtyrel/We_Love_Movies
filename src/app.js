0ebb53c9a6ff743674b2af46e12ddea70639d1bb
jaredtyrel <jaredtyrel@hotmail.com>
2022-12-01 18:04:28 -0800

    3rd commit

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
+const app = express();
+const cors = require("cors")
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
 
-const app = express();
-
 app.use(cors());
 app.use(express.json());
+
+const router = express.Router()
+router.get("/", cors(), (req, res) => {
+    res.json({ message: "Welcome! You can access the data using these routes: /movies, /movies/:moviesId, /reviews, /reviews/:reviewId, /theaters, /movies/:movieId/reviews, /movies/:movieId/reviews/:reviewId" })
+})
 
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
