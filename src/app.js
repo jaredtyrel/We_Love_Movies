2da2370768ca5189acd2ea5509eba9d888fba6cb
jaredtyrel <jaredtyrel@hotmail.com>
2022-12-02 08:03:11 -0800

    update app.js

--- src/app.js
+++ src/app.js
@@ -11,15 +11,9 @@ const theatersRouter = require("./theaters/theaters.router");
 app.use(cors());
 app.use(express.json());
 
-const router = express.Router()
-router.get("/", cors(), (req, res) => {
-    res.json({ message: "Welcome! You can access the data using these routes: /movies, /movies/:moviesId, /reviews, /reviews/:reviewId, /theaters, /movies/:movieId/reviews, /movies/:movieId/reviews/:reviewId" })
-})
-
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
 app.use("/theaters", theatersRouter);
-
 
 app.use(notFound);
 app.use(errorHandler);
